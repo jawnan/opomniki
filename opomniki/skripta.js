e1706677a055eabdd1f4046d5d4add6862ad3a34
Nejc Kadivnik <nejc.kadivnik@gmail.com>
2016-03-10 16:52:30 +0000

    Implementirana prijava

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -1,11 +1,20 @@
 window.addEventListener('load', function() {
 	//stran nalozena
-		
+	
+	var izvediPrijavo = function(){
+		var input = document.querySelector("#uporabnisko_ime").value;
+		document.querySelector("#uporabnik").innerHTML = input;
+		document.querySelector(".pokrivalo").style.visibility = "hidden";
+	}
+	//Izvedi prijavo
+	document.querySelector("#prijavniGumb").addEventListener('click', izvediPrijavo);
+	
+	
 	//Posodobi opomnike
 	var posodobiOpomnike = function() {
 		var opomniki = document.querySelectorAll(".opomnik");
 		
-		for (i = 0; i < opomniki.length; i++) {
+		for (var i = 0; i < opomniki.length; i++) {
 			var opomnik = opomniki[i];
 			var casovnik = opomnik.querySelector("span");
 			var cas = parseInt(casovnik.innerHTML);
